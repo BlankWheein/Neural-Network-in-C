5ba5fd4ccf6f34f4560f13b25f07b8bd22396284
ParkEunBlank <47646799+ParkEunBlank@users.noreply.github.com>
2020-09-03 12:44:30 +0200

    123

--- Neural Network in C/main.c
+++ Neural Network in C/main.c
@@ -14,20 +14,6 @@ double init_weight()
 {
     return ((double)rand())/((double)TMP_MAX);
 }
-void shuffle(int *array, size_t n)
-{
-    if (n > 1)
-    {
-        size_t i;
-        for (i = 0; i < n - 1; i++)
-        {
-          size_t j = i + rand() / (TMP_MAX / (n - i) + 1);
-          int t = array[j];
-          array[j] = array[i];
-          array[i] = t;
-        }
-    }
-}
 
 int main()
 {
